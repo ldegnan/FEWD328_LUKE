ad07a82faa4545fc8ca144dda3191a48be97a20c
ldegnan <ldegnan@devp-macbair2-m.local>
2017-06-06 11:21:06 -0400

    Final Project - Luke Degnan

---
+++ final project/elizabeth hoy dot com/js/modal.js
@@ -0,0 +1,67 @@
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+/*  broken left and right key functionality 
+
+document.onkeydown = function(e) {
+        e = e || window.event;
+        if (e.keyCode == '37') {
+            showSlides(-1) 
+        } else if (e.keyCode == '39') {
+            showSlides(1)
+        }
+}
+
+*/
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+/* broken swipe stuff
+
+$( document ).on( "swipeleft", ".ui-page", function( event ) {
+  var i2;
+  var slides2 = document.getElementsByClassName("mySlides");
+  var dots2 = document.getElementsByClassName("demo");
+  if (n > slides2.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+  dots2[slideIndex2-1].className += " active";
+});
+
+*/
